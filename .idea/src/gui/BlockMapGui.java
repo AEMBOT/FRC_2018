825331b7b24c88d3a39de75288be88f09875a6db
Goirick Saha <sahagoirick@gmail.com>
2017-12-14 16:31:47 -0800

    Save before merging in SMB's branch

--- .idea/src/gui/BlockMapGui.java
+++ .idea/src/gui/BlockMapGui.java
@@ -1,3 +1,4 @@
+package gui;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
---
+++ .idea/src/testing/TestApp.java
@@ -0,0 +1,35 @@
+package testing;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+import reckoning.WheelBasedReckoningTest;
+import real_time_model.RealTimeModelTest;
+
+public class TestApp {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Choose a test: ");
+        System.out.println();
+        System.out.println("1) WheelBasedReckoning Test");
+        System.out.println("2) RealTimeModel Test");
+
+        Scanner chooseTest = new Scanner(System.in);
+        int testNum = chooseTest.nextInt();
+
+        switch (testNum) {
+            case 1:
+                WheelBasedReckoningTest wbr_test = new WheelBasedReckoningTest();
+                wbr_test.doTest();
+                break;
+            case 2:
+                RealTimeModelTest rtm_test = new RealTimeModelTest();
+                rtm_test.do_test();
+                break;
+            case 3:
+                BlockMapGui bm = new BlockMapGui();
+                break;
+        }
+
+        chooseTest.close();
+    }
+}
