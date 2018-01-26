f6c6b63ee59f936b684fd073a8ce177ba5500b3b
Christopher Medlin <christopherjmedlin@gmail.com>
2018-01-25 17:15:40 -0800

    Set up CameraServer in robotInit method

--- src/org/usfirst/frc/falcons6443/robot/Robot.java
+++ src/org/usfirst/frc/falcons6443/robot/Robot.java
@@ -4,10 +4,11 @@ import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+import edu.wpi.first.wpilibj.CameraServer;
+
 import org.usfirst.frc.falcons6443.robot.commands.TeleopMode;
 import org.usfirst.frc.falcons6443.robot.subsystems.DriveTrainSystem;
 import org.usfirst.frc.falcons6443.robot.subsystems.NavigationSystem;
-
 import org.usfirst.frc.falcons6443.robot.commands.Delay;
 
 
@@ -38,6 +39,8 @@ public class Robot extends IterativeRobot {
         oi = new OI();
         autonomy = new Delay(1);
         teleop = new TeleopMode();
+
+        CameraServer.getInstance().startAutomaticCapture();
     }
 
     /*
