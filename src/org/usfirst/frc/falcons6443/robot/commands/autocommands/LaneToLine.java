101c1eb4f4e4dada41e24102b75326f217ef0223
aleks <bobopirate1010@gmail.com>
2018-02-07 16:30:28 -0800

    Restructure.

--- src/org/usfirst/frc/falcons6443/robot/commands/autocommands/LaneToLine.java
+++ src/org/usfirst/frc/falcons6443/robot/commands/autocommands/LaneToLine.java
@@ -1,9 +1,11 @@
-package org.usfirst.frc.falcons6443.robot.commands;
+package org.usfirst.frc.falcons6443.robot.commands.autocommands;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
+import org.usfirst.frc.falcons6443.robot.commands.subcommands.DriveToDistance;
 
 /**
  * Command to get over AutoLine from left or right starting position
+ *
  *
  * @author Owen Engbretson
  */
