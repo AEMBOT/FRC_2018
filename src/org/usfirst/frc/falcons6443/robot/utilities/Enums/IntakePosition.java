78d656bce378981c4bd1d182ed710cd881e222ac
LizzyMilford <Lizzym@outofthisworld.net>
2018-03-13 18:19:45 -0700

    -don't look at this

--- src/org/usfirst/frc/falcons6443/robot/utilities/Enums/IntakePosition.java
+++
@@ -1,5 +0,0 @@
-package org.usfirst.frc.falcons6443.robot.utilities.Enums;
-
-public enum IntakePosition {
-    IntakeUpPosition, IntakeDownPosition
-}
--- src/org/usfirst/frc/falcons6443/robot/utilities/Enums/XboxRumble.java
+++
@@ -1,5 +0,0 @@
-package org.usfirst.frc.falcons6443.robot.utilities.Enums;
-
-public enum XboxRumble {
-    RumbleLeft, RumbleRight, RumbleBoth
-}
