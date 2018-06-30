dde24f05d85fa5d343498bb1dbbc83a366adaec0
Christopher Medlin <aembot6443@gmail.com>
2018-06-29 17:15:38 -0700

    Second try is the charm...?

--- src/org/usfirst/frc/falcons6443/robot/Robot.java
+++ src/org/usfirst/frc/falcons6443/robot/Robot.java
@@ -21,6 +21,7 @@ import org.usfirst.frc.falcons6443.robot.utilities.*;
 public class Robot extends IterativeRobot {
 
     //Team laptop was here! (Test for the new 6443driverstation account)
+    //Second try is the charm...?
 
     // All the subsystems that the robot possesses
     // If a new subsystem is added, it must also be added to SimpleCommand.
